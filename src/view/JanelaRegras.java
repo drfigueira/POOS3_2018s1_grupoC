7e8fb8d1b4dcd088dde1f6fd05f826365b48a3e4
Igor Gabriel <igor_g_s_a@hotmail.com>
2018-06-13 11:46:27 -0300

    Foi criado janela para inserir as regras

---
+++ src/view/JanelaRegras.java
@@ -0,0 +1,16 @@
+package view;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class JanelaRegras extends JInternalFrame implements ActionListener {
+    public JanelaRegras(){
+        super("Regras");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
